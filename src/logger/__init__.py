64635707321b68b47ed4031aa0d4d59e879d5ea3
haseeb1511 <haseebmanzoor1511@gmail.com>
2025-06-20 10:27:19 +0500

    logs code file created

---
+++ src/logger/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import os
+from from_root import from_root
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+
+LOG_DIR = "logs"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+MAX_LOG_SIZE = 5*1024*1024  # 5MB
+BACKUP_COUNT = 3
+
+log_dir_path = os.path.join(from_root(),LOG_DIR)
+os.makedirs(log_dir_path,exist_ok=True)
+log_file_path = os.path.join(log_dir_path,LOG_FILE)
+
+def configure_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    file_handler = RotatingFileHandler(filename=log_file_path,maxBytes=MAX_LOG_SIZE,backupCount=BACKUP_COUNT)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    consol_handler = logging.StreamHandler()
+    consol_handler.setFormatter(formatter)
+    consol_handler.setLevel(logging.DEBUG)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(consol_handler)
+
+    return logger
